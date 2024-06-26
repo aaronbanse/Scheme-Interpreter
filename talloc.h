ea80218375f72b5ed5d662667188a380475086da
Aaron Banse <acbanse@icloud.com>
2024-06-26 14:58:48 -0500

    Update talloc.h comments

--- talloc.h
+++ talloc.h
@@ -4,11 +4,7 @@
 #ifndef TALLOC_H
 #define TALLOC_H
 
-// Replacement for malloc that stores the pointers allocated. It should store
-// the pointers in some kind of list; a linked list would do fine, but insert
-// here whatever code you'll need to do so; don't call functions in the
-// pre-existing linkedlist.h. Otherwise you'll end up with circular
-// dependencies, since you're going to modify the linked list to use talloc.
+// Replacement for malloc that stores the pointers allocated so they can be freed easily later.
 void *talloc(size_t size);
 
 // Free all pointers allocated by talloc, as well as whatever memory you
@@ -16,8 +12,7 @@ void *talloc(size_t size);
 void tfree();
 
 // Replacement for the C function "exit", that consists of two lines: it calls
-// tfree before calling exit. It's useful to have later on; if an error happens,
-// you can exit your program, and all memory is automatically cleaned up.
+// tfree before calling exit.
 void texit(int status);
 
 #endif
